918b30cf55ae1f57c3030293b98599fd025dfaf2
karin-jpg <andradkarin@gmail.com>
2021-04-23 22:47:03 -0300

    Instalação da biblioteca body-parser
    Recebimento de informações via post

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     //o render automaticamente acessa a pasta view, nao necessário coloca-la no caminho
@@ -12,6 +15,14 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar.ejs");
+});
+
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    
+    res.send("Titulo: "+titulo+ " Descricao: "+ descricao);
 });
 
 app.listen(3000, () => {
